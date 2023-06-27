d60d3e2650410b38caa35df11e1e62da3f8d9e75
Carlos Acevedo <cacevedod@devco.com.co>
2023-06-27 16:39:59 -0500

    pruebas para el banner

--- src/app/banner/banner.component.spec.ts
+++ src/app/banner/banner.component.spec.ts
@@ -5,17 +5,51 @@ import { BannerComponent } from './banner.component';
 describe('BannerComponent', () => {
   let component: BannerComponent;
   let fixture: ComponentFixture<BannerComponent>;
+  let h1: HTMLElement;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BannerComponent]
+  describe('Ejemplo usando 2 beforeEach', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ BannerComponent ],
+      }).compileComponents();
     });
-    fixture = TestBed.createComponent(BannerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BannerComponent);
+      component = fixture.componentInstance;
+      h1 = fixture.nativeElement.querySelector('h1');
+    });
+
+    tests();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Ejemplo usando 1 beforeEach', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ BannerComponent ],
+      }).compileComponents();
+      fixture = TestBed.createComponent(BannerComponent);
+      component = fixture.componentInstance;
+      h1 = fixture.nativeElement.querySelector('h1');
+    });
+
+    tests();
   });
+
+  function tests() {
+    it('ejemplo de que no se encuentran los titulos sin usar detectChanges()', () => {
+      expect(h1.textContent).toEqual('');
+    });
+
+    it('debe mostrar el titulo cargado del componente', () => {
+      fixture.detectChanges();
+      expect(h1.textContent).toContain(component.title);
+    });
+
+    it('debe mostrar un titulo diferente si se cambia el titulo del componente', () => {
+      component.title = 'Titulo de prueba';
+      fixture.detectChanges();
+      expect(h1.textContent).toContain('Titulo de prueba');
+    });
+  }
 });
